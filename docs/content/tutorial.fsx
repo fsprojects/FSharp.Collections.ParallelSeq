b2e99b7383d877f12edabd8bc0803f37b5d97370
F# Software Foundation <fsgit@fastmail.fm>
2014-04-18 19:55:40 +0100

    tests

--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -12,6 +12,13 @@ Here is an example of using the parallel sequence combinators:
 #r "FSharp.Collections.ParallelSeq.dll"
 open FSharp.Collections.ParallelSeq
 
+let isPrime n = 
+    let upperBound = int (sqrt (float n))
+    let hasDivisor =     
+        [2..upperBound]
+        |> List.exists (fun i -> n % i = 0)
+    not hasDivisor
+        
 let nums = [|1..500000|]
 let finalDigitOfPrimes = 
         nums 
@@ -19,3 +26,16 @@ let finalDigitOfPrimes =
         |> PSeq.groupBy (fun i -> i % 10)
         |> PSeq.map (fun (k, vs) -> (k, Seq.length vs))
         |> PSeq.toArray  
+
+let averageOfFinalDigit = 
+    nums 
+    |> PSeq.filter isPrime
+    |> PSeq.groupBy (fun i -> i % 10)
+    |> PSeq.map (fun (k, vs) -> (k, Seq.length vs))
+    |> PSeq.averageBy (fun (k,n) -> float n)
+
+let sumOfLastDigitsOfPrimes = 
+    nums 
+    |> PSeq.filter isPrime
+    |> PSeq.sumBy (fun x -> x % 10)
+
--- src/FSharp.Collections.ParallelSeq/AssemblyInfo.fs
+++ src/FSharp.Collections.ParallelSeq/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Collections.ParallelSeq")>]
 [<assembly: AssemblyProductAttribute("FSharp.Collections.ParallelSeq")>]
 [<assembly: AssemblyDescriptionAttribute("Parallel sequence operators for F#.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "1.0.0"
