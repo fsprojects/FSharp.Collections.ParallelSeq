bc6953da320f28763a209a3590372034b7eb5dad
Sergey Tihon <sergey.tihon@gmail.com>
2014-10-11 08:57:23 +0300

    Fixed link to NuGet package

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -16,7 +16,7 @@ let info =
     "project-author", "Your Name"
     "project-summary", "A short summary of your project"
     "project-github", githubLink
-    "project-nuget", "http://nuget.com/packages/FSharp.Collections.ParallelSeq" ]
+    "project-nuget", "https://www.nuget.org/packages/FSharp.Collections.ParallelSeq" ]
 
 // --------------------------------------------------------------------------------------
 // For typical project, no changes are needed below
